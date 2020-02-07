df1b28812502421f2c6f8e079854be2d898d2b8a
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-02-07 17:13:19 +0100

    [ADD] parcelize implementation

--- app/src/main/java/com/accenture/signify/data/source/remote/CategoryModels.kt
+++ app/src/main/java/com/accenture/signify/data/source/remote/CategoryModels.kt
@@ -1,24 +1,24 @@
-package com.accenture.domain.model
+package com.accenture.signify.data.source.remote
 
-data class Message(
-    val categories: List<Category>
-)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-
-data class Category(
-    val categoryProducts: List<Product>,
+@Parcelize
+data class CategoryParcelable(
+    val categoryProducts: List<ProductParcelable>,
     val categoryEnergySave: String,
     val categoryIndex: Int,
     val categoryName: String,
     val categoryImage: String,
     val categoryPrice: String
-)
+) : Parcelable
 
-data class Product(
+@Parcelize
+data class ProductParcelable(
     val productImage: List<String>,
     val productName: String,
     val productDescription: String,
     val productSpecOne: String,
     val productSpecThree: String,
     val productScene: String
-)
+) : Parcelable
---
+++ app/src/main/java/com/accenture/signify/ui/lightfinder/ProductsFragment.kt
@@ -0,0 +1,11 @@
+package com.accenture.signify.ui.lightfinder
+
+import androidx.fragment.app.Fragment
+
+
+class ProductsFragment : Fragment(){
+
+    companion object {
+        const val PRODUCTS_CATEGORY_ID_KEY = "ProductsFragment::id"
+    }
+}
