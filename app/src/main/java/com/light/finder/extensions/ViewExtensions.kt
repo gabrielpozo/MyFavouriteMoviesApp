c5883c26707634cb7d5976d3318d4ef928cccfaa
ege <ege.kuzubasioglu@accenture.com>
2020-02-28 10:29:21 +0100

    [ADD] safe click listener for shutter button

--- app/src/main/java/com/light/finder/extensions/ViewExtensions.kt
+++ app/src/main/java/com/light/finder/extensions/ViewExtensions.kt
@@ -1,37 +1,14 @@
 package com.light.finder.extensions
 
-import android.os.Build
-import android.view.DisplayCutout
 import android.view.View
-import android.view.WindowManager
-import android.widget.ImageButton
 import android.widget.ImageView
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AlertDialog
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import java.io.File
 
-const val FLAGS_FULLSCREEN =
-    View.SYSTEM_UI_FLAG_LOW_PROFILE or
-            View.SYSTEM_UI_FLAG_FULLSCREEN or
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
 const val ANIMATION_FAST_MILLIS = 50L
 const val ANIMATION_SLOW_MILLIS = 100L
-
-fun ImageButton.simulateClick(delay: Long = ANIMATION_FAST_MILLIS) {
-    performClick()
-    isPressed = true
-    invalidate()
-    postDelayed({
-        invalidate()
-        isPressed = false
-    }, delay)
-}
 
 fun View.visible() {
     visibility = View.VISIBLE
@@ -43,34 +20,6 @@ fun View.invisible() {
 
 fun View.gone() {
     visibility = View.GONE
-}
-
-@RequiresApi(Build.VERSION_CODES.P)
-fun View.padWithDisplayCutout() {
-
-    fun doPadding(cutout: DisplayCutout) = setPadding(
-        cutout.safeInsetLeft,
-        cutout.safeInsetTop,
-        cutout.safeInsetRight,
-        cutout.safeInsetBottom
-    )
-
-    rootWindowInsets?.displayCutout?.let { doPadding(it) }
-
-    setOnApplyWindowInsetsListener { _, insets ->
-        insets.displayCutout?.let { doPadding(it) }
-        insets
-    }
-}
-
-fun AlertDialog.showImmersive() {
-    window?.setFlags(
-        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-    )
-    window?.decorView?.systemUiVisibility = FLAGS_FULLSCREEN
-    show()
-    window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
 }
 
 fun ImageView.loadFile(file: File) {
