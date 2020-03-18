3a0599984efbbdcd84bf33a153bf8b0b0ecad451
ege <ege.kuzubasioglu@accenture.com>
2020-03-18 15:31:43 +0100

    [WIP] dots

--- app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
+++ app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
@@ -1,7 +1,6 @@
 package com.light.finder.ui.lightfinder
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,11 +18,6 @@ import com.light.finder.ui.adapters.DetailImageAdapter
 import com.light.presentation.viewmodels.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_detail.*
 import kotlinx.android.synthetic.main.layout_detail_bottom_sheet.*
-import android.view.ViewTreeObserver.OnScrollChangedListener
-import com.light.finder.extensions.gone
-import androidx.core.widget.NestedScrollView
-
-
 
 
 class DetailFragment : Fragment() {
@@ -118,11 +112,32 @@ class DetailFragment : Fragment() {
 
     private fun setViewPager(product: Product) {
         //todo set viewpager with images
+        //addBottomDots(0, product.imageUrls)
         val myList: MutableList<String> = mutableListOf()
         myList.addAll(product.imageUrls)
         myList.add("https://s3.us-east-2.amazonaws.com/imagessimonprocessed/HAL_A19_E26_FROSTED.jpg")
         viewPagerDetail.adapter = DetailImageAdapter(requireContext(), myList)
     }
+
+    /*private fun addBottomDots(currentPage: Int, layouts: List<String>) {
+
+        val dots = arrayOfNulls<TextView>(layouts.size)
+
+        val colorsActive = resources.getIntArray(R.array.array_dot_active)
+        val colorsInactive = resources.getIntArray(R.array.array_dot_inactive)
+
+        dotsLayout.removeAllViews()
+        for (i in 0 until dots.length) {
+            dots[i] = TextView(this)
+            dots[i].setText(Html.fromHtml("&#8226;"))
+            dots[i].setTextSize(35)
+            dots[i].setTextColor(colorsInactive[currentPage])
+            dotsLayout.addView(dots[i])
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage])
+    }*/
 
 
 }
