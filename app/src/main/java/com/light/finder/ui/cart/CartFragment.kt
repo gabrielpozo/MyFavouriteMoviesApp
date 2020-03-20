7d986c9c0ede556400678f364541d95c2ef56f5d
ege <ege.kuzubasioglu@accenture.com>
2020-03-20 11:00:33 +0100

    [ADD] Cart fragment

--- app/src/main/java/com/light/finder/ui/cart/CartFragment.kt
+++ app/src/main/java/com/light/finder/ui/cart/CartFragment.kt
@@ -4,18 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.light.finder.R
+import android.webkit.WebViewClient
 import com.light.finder.ui.BaseFragment
+import kotlinx.android.synthetic.main.cart_fragment.*
+import timber.log.Timber
+
 
 class CartFragment : BaseFragment() {
-    companion object;
+    companion object {
+        const val URL = "https://www.store.lightguide.signify.com/us/checkout/cart/"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.empty_fragment, container, false)
+        return inflater.inflate(com.light.finder.R.layout.cart_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadWebView(URL)
+
+
+    }
+
+    private fun loadWebView(url: String) {
+        try {
+            webView.webViewClient = WebViewClient()
+            webView.settings.setSupportZoom(true)
+            webView.settings.allowContentAccess = true
+            webView.settings.builtInZoomControls = true
+            webView.settings.displayZoomControls = false
+
+            webView.loadUrl(url)
+        } catch (e: Exception) {
+            Timber.w("setUpNavigationView")
+        }
+
     }
 
 
