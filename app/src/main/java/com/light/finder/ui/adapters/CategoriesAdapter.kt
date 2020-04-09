7cd2f97d46e7787ae048ad56ae472406b94467e5
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-04-09 11:42:34 +0200

    [ADD] clean adapter most efficient option check-logic

--- app/src/main/java/com/light/finder/ui/adapters/CategoriesAdapter.kt
+++ app/src/main/java/com/light/finder/ui/adapters/CategoriesAdapter.kt
@@ -19,6 +19,7 @@ class CategoriesAdapter(private val listener: (Category) -> Unit) :
         emptyList(),
         areItemsTheSame = { old, new -> old.categoryIndex == new.categoryIndex }
     )
+    private val maxEnergySaving = categories.maxBy { it.maxEnergySaving }?.maxEnergySaving
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = parent.inflate(R.layout.item_category, false)
@@ -31,19 +32,17 @@ class CategoriesAdapter(private val listener: (Category) -> Unit) :
         val category = categories[position]
 
 
-        holder.bind(category,  categories.maxBy { it.maxEnergySaving }?.maxEnergySaving!!)
+        holder.bind(category, maxEnergySaving ?: -0.0f)
         holder.itemView.setOnClickListener { listener(category) }
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         @SuppressLint("SetTextI18n")
         fun bind(category: Category, maxSaving: Float) {
-            var colorText = ""
             itemView.category_name.text = category.categoryName
             itemView.price.text = category.priceRange
             itemView.bulbCover.loadUrl(category.categoryImage)
             category.maxEnergySaving
-            //TODO it will change once we have the field on the api(most efficient option)
             if (maxSaving == category.maxEnergySaving) {
                 itemView.energyButton.visibility = View.VISIBLE
             }
@@ -58,7 +57,7 @@ class CategoriesAdapter(private val listener: (Category) -> Unit) :
                     LinearLayout.LayoutParams.WRAP_CONTENT
                 )
                 textView.setTextAppearance(R.style.SubTitleField)
-                if(index < category.colors.size -1) {
+                if (index < category.colors.size - 1) {
                     textView.setPadding(0, 0, 0, 36)
                 }
                 textView.compoundDrawablePadding = 32
