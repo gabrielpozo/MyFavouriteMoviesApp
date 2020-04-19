995733ebc5e3103c1cdc92be08e25a77aa5efe40
ege <ege.kuzubasioglu@accenture.com>
2020-04-19 16:36:58 +0200

    [FIX] cookie store crash

--- app/src/main/java/com/light/finder/data/source/remote/SharedPreferencesCookieStore.kt
+++ app/src/main/java/com/light/finder/data/source/remote/SharedPreferencesCookieStore.kt
@@ -37,7 +37,9 @@ open class SharedPreferencesCookieStore(
     override fun removeAll(): Boolean {
         super.removeAll()
 
-        preferences.edit().clear().apply()
+        synchronized(this) {
+            preferences.edit().clear().apply()
+        }
         return true
     }
 
@@ -47,9 +49,10 @@ open class SharedPreferencesCookieStore(
         uri?.let {
             val index = getEffectiveURI(uri)
             val cookies = uriIndex[index]
-            //TODO Check if crash Fatal Exception: java.util.ConcurrentModificationException solved
             try {
-                preferences.edit().putString(index.toString(), gson.toJson(cookies)).commit()
+                synchronized(this) {
+                    preferences.edit().putString(index.toString(), gson.toJson(cookies)).apply()
+                }
             } catch (e:Exception) {
                 Log.e("ERROR", "ConcurrentModificationException")
             }
@@ -64,13 +67,15 @@ open class SharedPreferencesCookieStore(
             val index = getEffectiveURI(uri)
             val cookies = uriIndex[index]
 
-            preferences.edit().apply {
-                if (cookies == null) {
-                    remove(index.toString())
-                } else {
-                    putString(index.toString(), gson.toJson(cookies))
-                }
-            }.apply()
+            synchronized(this) {
+                preferences.edit().apply {
+                    if (cookies == null) {
+                        remove(index.toString())
+                    } else {
+                        putString(index.toString(), gson.toJson(cookies))
+                    }
+                }.apply()
+            }
         }
 
         return result
