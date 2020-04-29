11ddd76880763b9636735f4d95487ac8efc83105
ege <ege.kuzubasioglu@accenture.com>
2020-04-29 11:18:36 +0200

    [UPDATE] analytics consent

--- app/src/main/java/com/light/finder/common/PrefManager.kt
+++ app/src/main/java/com/light/finder/common/PrefManager.kt
@@ -10,6 +10,7 @@ class PrefManager(_context: Context) {
         private const val PREF_NAME = "sharedPref"
 
         private const val IS_TERMS_ACCEPTED = "isTermsAccepted"
+        private const val IS_CONSENT_ACCEPTED = "isConsentAccepted"
     }
     private var pref: SharedPreferences
     private var editor: SharedPreferences.Editor
@@ -20,6 +21,13 @@ class PrefManager(_context: Context) {
         get() = pref.getBoolean(IS_TERMS_ACCEPTED, false)
         set(isAccepted) {
             editor.putBoolean(IS_TERMS_ACCEPTED, isAccepted)
+            editor.commit()
+        }
+
+    var isConsentAccepted: Boolean
+        get() = pref.getBoolean(IS_CONSENT_ACCEPTED, false)
+        set(isAccepted) {
+            editor.putBoolean(IS_CONSENT_ACCEPTED, isAccepted)
             editor.commit()
         }
 
