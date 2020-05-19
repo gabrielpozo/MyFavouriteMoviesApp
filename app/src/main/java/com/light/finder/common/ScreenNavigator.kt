07c84a1b59c615908b5bf4caa9504875235d18d3
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-05-19 16:49:49 +0200

    [FIX] remove log

--- app/src/main/java/com/light/finder/common/ScreenNavigator.kt
+++ app/src/main/java/com/light/finder/common/ScreenNavigator.kt
@@ -2,7 +2,6 @@ package com.light.finder.common
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.light.domain.model.Category
