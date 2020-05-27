3b923908695632574f8d45d3c1c26be945887035
ege <ege.kuzubasioglu@accenture.com>
2020-05-27 09:27:06 +0200

    [FIX] lateinit property crash

--- app/src/main/java/com/light/finder/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/light/finder/ui/camera/CameraFragment.kt
@@ -71,7 +71,6 @@ class CameraFragment : BaseFragment() {
     private lateinit var cameraProvider: ProcessCameraProvider
     private lateinit var controls: View
     private var modelUiState: ModelStatus = ModelStatus.FEED
-    //private var rotationDegree = 200
 
 
     val timer = object : CountDownTimer(INIT_INTERVAL, DOWN_INTERVAL) {
@@ -675,7 +674,7 @@ class CameraFragment : BaseFragment() {
 
     //TODO set this method for extension
     private fun initializeLottieAnimation() {
-        lottieAnimationView.progress = 0.0f
+        lottieAnimationView?.progress = 0.0f
     }
 
     override fun onDestroyView() {
@@ -684,12 +683,12 @@ class CameraFragment : BaseFragment() {
     }
 
     fun disableCameraCaptureButton() {
-        controls.cameraCaptureButton.isEnabled = false
+        controls.cameraCaptureButton?.isEnabled = false
 
     }
 
     fun enableCameraCaptureButton() {
-        controls.cameraCaptureButton.isEnabled = true
+        controls.cameraCaptureButton?.isEnabled = true
     }
 }
 
