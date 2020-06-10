58eab177a7a4095756934f0f762cc00941f81070
ege <ege.kuzubasioglu@accenture.com>
2020-06-10 10:44:06 +0200

    [FIX] nested scroll issue [WIP] move title and price to layout_filter_dialog.xml

--- app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
+++ app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.view.marginBottom
@@ -137,7 +138,7 @@ class DetailFragment : BaseFragment() {
     }
 
     private fun setBottomSheetBehaviour() {
-        val bottomSheetLayout = view?.findViewById<NestedScrollView>(R.id.bottomSheetLayout)
+        val bottomSheetLayout = view?.findViewById<LinearLayout>(R.id.bottomSheetLayout)
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout)
 
         context?.let {
