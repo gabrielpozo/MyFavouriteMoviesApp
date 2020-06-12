1143a9308f3a5be8deec44c7d366b74d154fc717
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-06-12 14:53:42 +0200

    [ADD] remove space on populate data

--- app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
+++ app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
@@ -406,7 +406,6 @@ class DetailFragment : BaseFragment() {
     }
 
     private fun populateStickyHeaderData(product: Product) {
-
         // product sticky header
         val bannerPacks = String.format(
             getString(R.string.banner_packs),
