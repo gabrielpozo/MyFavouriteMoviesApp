163b45f84072677ccb2792efba8191eec8fa887b
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-06-17 10:47:03 +0200

    [ADD] reverse color ordering

--- usecases/src/main/java/com/light/usecases/GetColorVariationsUseCase.kt
+++ usecases/src/main/java/com/light/usecases/GetColorVariationsUseCase.kt
@@ -55,7 +55,9 @@ class GetColorVariationsUseCase : BaseUseCase<List<FilterVariationCF>>() {
             }
         }
 
-        return DataState.Success(filterHashSet.toList())
+        return DataState.Success(filterHashSet.sortedWith(Comparator { f1, f2 ->
+            (f1?.codeFilter ?: -1) - (f2?.codeFilter ?: -1)
+        }).toList())
     }
 
 }
