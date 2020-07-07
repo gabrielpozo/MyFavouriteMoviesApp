1fff09c48916e740dbdee746dde6cf7d2aac4d2c
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-07-07 15:58:24 +0200

    [ADD] bug fixed - 1500

--- app/src/main/java/com/light/finder/ui/adapters/FilterVariationsAdapter.kt
+++ app/src/main/java/com/light/finder/ui/adapters/FilterVariationsAdapter.kt
@@ -167,7 +167,13 @@ class FilterFinishAdapter(
                 legendTag = FINISH_LEGEND_TAG
             )
             itemView.setDrawableOnBackground(filter)
-            itemView.setFinishVariation(filter.codeFilter)
+
+            itemView.imageFilterCover.loadCircleImage(getLegendFinishTagPrefImage(
+                filter.codeFilter,
+                filterTypeList = filterFinishList,
+                legendTag = FINISH_LEGEND_TAG
+            ))
+            //itemView.setFinishVariation(filter.codeFilter)
         }
     }
 }
