11b763d07ecd11723f4eb4432904f799b8177fcd
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-07-20 13:17:45 +0200

    [ADD] adding data product List to ViewModels

--- presentation/src/main/java/com/light/presentation/viewmodels/LoadingBrowsingProductsViewModel.kt
+++ presentation/src/main/java/com/light/presentation/viewmodels/LoadingBrowsingProductsViewModel.kt
@@ -2,23 +2,24 @@ package com.light.presentation.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.light.domain.model.ProductBrowsing
 import com.light.usecases.RequestBrowsingProductsUseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
 
-class LoadingBrowsingProductsViewModel(
+class FittingBrowsingViewModel(
     private val requestBrowsingProductsUseCase: RequestBrowsingProductsUseCase,
     uiDispatcher: CoroutineDispatcher
 ) : BaseViewModel(uiDispatcher) {
 
     sealed class LoadingBrowsingState {
-        object SuccessRequestStatus : LoadingBrowsingState()
+        data class SuccessRequestStatus(private val productBrowsingList: List<ProductBrowsing>) :
+            LoadingBrowsingState()
+
         data class ErrorRequestStatus(private val message: String) : LoadingBrowsingState()
-
     }
-
 
     val loadingBrowsingLiveData: LiveData<LoadingBrowsingState>
         get() = _loadingBrowsingLiveData
@@ -34,9 +35,9 @@ class LoadingBrowsingProductsViewModel(
         }
     }
 
-    private fun onSuccessRequest() {
-
-        _loadingBrowsingLiveData.value = LoadingBrowsingState.SuccessRequestStatus
+    private fun onSuccessRequest(productBrowsingList: List<ProductBrowsing>) {
+        _loadingBrowsingLiveData.value =
+            LoadingBrowsingState.SuccessRequestStatus(productBrowsingList)
     }
 
     private fun onErrorRequest(exception: Exception, message: String) {
