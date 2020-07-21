dc65353043b30e425ba08030aaaf272d4c017907
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-07-21 11:06:46 +0200

    [ADD] setting update ui method on fitting view

--- app/src/main/java/com/light/finder/ui/browse/BrowseFittingFragment.kt
+++ app/src/main/java/com/light/finder/ui/browse/BrowseFittingFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.light.finder.R
 import com.light.finder.di.modules.submodules.BrowseFittingModule
 import com.light.finder.di.modules.submodules.BrowsingFittingComponent
 import com.light.finder.extensions.getViewModel
 import com.light.presentation.viewmodels.BrowseFittingViewModel
+import com.light.presentation.viewmodels.BrowseFittingViewModel.*
 
 class BrowseFittingFragment : BaseFilteringFragment() {
 
@@ -29,6 +31,30 @@ class BrowseFittingFragment : BaseFilteringFragment() {
         super.onViewCreated(view, savedInstanceState)
         activity?.run {
             component = browseComponent.plus(BrowseFittingModule())
+        }
+
+        setObservers()
+    }
+
+    private fun setObservers() {
+        viewModel.modelBrowsingLiveData.observe(
+            viewLifecycleOwner,
+            Observer(::updateBrowsingFittingUI)
+        )
+    }
+
+    private fun updateBrowsingFittingUI(modelBrowse: UiBrowsingModel) {
+        when (modelBrowse) {
+            is UiBrowsingModel.SuccessRequestStatus -> {
+
+            }
+            is UiBrowsingModel.ErrorRequestStatus -> {
+
+            }
+
+            is UiBrowsingModel.LoadingStatus -> {
+
+            }
         }
     }
 }
