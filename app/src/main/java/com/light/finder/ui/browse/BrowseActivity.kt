675dfc36a43f9d2a29267713c6eb8875173aa3f7
ege <ege.kuzubasioglu@accenture.com>
2020-07-21 11:00:47 +0200

    [UPDATE] compiling

--- app/src/main/java/com/light/finder/ui/browse/BrowseActivity.kt
+++ app/src/main/java/com/light/finder/ui/browse/BrowseActivity.kt
@@ -1,11 +1,13 @@
 package com.light.finder.ui.browse
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.light.finder.BaseLightFinderActivity
 import com.light.finder.R
+import kotlinx.android.synthetic.main.activity_browse.*
 
 class BrowseActivity : BaseLightFinderActivity(){
 
@@ -24,6 +26,8 @@ class BrowseActivity : BaseLightFinderActivity(){
 
         setContentView(R.layout.activity_browse)
         container = findViewById(R.id.fragment_container_browse)
+
+        textResetSkip.paintFlags = textResetSkip.paintFlags or Paint.UNDERLINE_TEXT_FLAG
 
 
 
--- app/src/main/java/com/light/finder/ui/browse/BrowseFittingFragment.kt
+++ app/src/main/java/com/light/finder/ui/browse/BrowseFittingFragment.kt
@@ -1,12 +1,9 @@
 package com.light.finder.ui.browse
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.light.finder.R
 import com.light.finder.ui.BaseFragment
 
