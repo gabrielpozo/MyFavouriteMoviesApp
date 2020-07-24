4c1269d4f700dcea5661198e58af2b70c8edae31
ege <ege.kuzubasioglu@accenture.com>
2020-07-24 14:09:02 +0200

    [FIX] bottom sheet

--- app/src/main/java/com/light/finder/ui/browse/BrowseShapeFragment.kt
+++ app/src/main/java/com/light/finder/ui/browse/BrowseShapeFragment.kt
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import androidx.core.view.ViewCompat
-import androidx.core.widget.NestedScrollView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.paris.extensions.backgroundRes
