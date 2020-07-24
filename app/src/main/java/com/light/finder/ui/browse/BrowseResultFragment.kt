cb18d144897077d714577305b35b08bffd471466
ege <ege.kuzubasioglu@accenture.com>
2020-07-24 11:51:23 +0200

    [ADD] browse result

--- app/src/main/java/com/light/finder/ui/browse/BrowseResultFragment.kt
+++ app/src/main/java/com/light/finder/ui/browse/BrowseResultFragment.kt
@@ -1,6 +1,5 @@
-package com.light.finder.ui.lightfinder
+package com.light.finder.ui.browse
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,29 +8,27 @@ import androidx.lifecycle.Observer
 import com.light.domain.model.Category
 import com.light.domain.model.Message
 import com.light.finder.R
-import com.light.finder.common.ActivityCallback
 import com.light.finder.data.source.local.LocalPreferenceDataSourceImpl
 import com.light.finder.data.source.remote.MessageParcelable
-import com.light.finder.di.modules.submodules.CategoriesComponent
-import com.light.finder.di.modules.submodules.CategoriesModule
+import com.light.finder.di.modules.submodules.BrowseResultComponent
+import com.light.finder.di.modules.submodules.BrowseResultModule
 import com.light.finder.extensions.*
 import com.light.finder.ui.BaseFragment
-import com.light.finder.ui.adapters.CategoriesAdapter
+import com.light.finder.ui.adapters.BrowseResultAdapter
 import com.light.presentation.common.Event
-import com.light.presentation.viewmodels.CategoryViewModel
+import com.light.presentation.viewmodels.BrowseResultViewModel
 import com.light.source.local.LocalPreferenceDataSource
 import kotlinx.android.synthetic.main.fragment_browse_result.*
 
-class CategoriesFragment : BaseFragment() {
+class BrowseResultFragment : BaseFragment() {
 
     companion object {
-        const val CATEGORIES_ID_KEY = "CategoriesFragment::id"
+        const val CATEGORIES_ID_KEY = "BrowseResultFragment::id"
     }
 
-    private lateinit var component: CategoriesComponent
-    private val viewModel: CategoryViewModel by lazy { getViewModel { component.categoryViewModel } }
-    private lateinit var adapter: CategoriesAdapter
-    private lateinit var activityCallback: ActivityCallback
+    private lateinit var component: BrowseResultComponent
+    private val viewModel: BrowseResultViewModel by lazy { getViewModel { component.browseResultViewModel } }
+    private lateinit var adapter: BrowseResultAdapter
     private val localPreferences: LocalPreferenceDataSource by lazy {
         LocalPreferenceDataSourceImpl(
             requireContext()
@@ -47,20 +44,12 @@ class CategoriesFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_browse_result, container, false)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        try {
-            activityCallback = context as ActivityCallback
-        } catch (e: ClassCastException) {
-            throw ClassCastException()
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         activity?.run {
-            component = lightFinderComponent.plus(CategoriesModule())
+            component = lightFinderComponent.plus(BrowseResultModule())
         } ?: throw Exception("Invalid Activity")
 
         arguments?.let { bundle ->
@@ -76,20 +65,15 @@ class CategoriesFragment : BaseFragment() {
     }
 
 
-    override fun onResume() {
-        super.onResume()
-        activityCallback.setBottomBarInvisibility(false)
-    }
-
     private fun navigationObserver() {
         viewModel.modelNavigation.observe(viewLifecycleOwner, Observer(::navigateToProductList))
     }
 
-    private fun updateUI(model: CategoryViewModel.Content) {
+    private fun updateUI(model: BrowseResultViewModel.Content) {
         updateData(model.messages, model.message)
     }
 
-    private fun navigateToProductList(navigationModel: Event<CategoryViewModel.NavigationModel>) {
+    private fun navigateToProductList(navigationModel: Event<BrowseResultViewModel.NavigationModel>) {
         navigationModel.getContentIfNotHandled()?.let { navModel ->
             screenNavigator.navigateToDetailScreen(navModel.category)
         }
@@ -111,14 +95,7 @@ class CategoriesFragment : BaseFragment() {
                     getString(R.string.text_results).getIntFormatter(categories.size)
             }
         }
-        /*textViewBulbType.ellipsize = TextUtils.TruncateAt.END
-        textViewBulbType.text =
-            getString(R.string.light_bulb_recognised_as).getStringFormatter(message.baseIdentified + " " + message.shapeIdentified)*/
-/*        getLegendFormFactorTag(
-            code = message.shapeIdentified.toInt(),
-            filterTypeList = localPreferences.loadFormFactorLegendTags(),
-            legendTag = ""
-        )*/
+
         textViewFitting.text = getString(R.string.based_on_s_fitting).format(
             getFormFactorIdTagName(
                 message.shapeIdentified,
@@ -136,7 +113,7 @@ class CategoriesFragment : BaseFragment() {
 
 
     private fun setAdapter(message: Message) {
-        adapter = CategoriesAdapter(
+        adapter = BrowseResultAdapter(
             viewModel::onCategoryClick,
             localPreferences.loadLegendCctFilterNames(),
             localPreferences.loadFormFactorLegendTags(),
