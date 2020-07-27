2928d5e6a57fd49ad204d349b0be10f842791fcd
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-07-27 11:43:46 +0200

    [ADD] animation back button added

--- app/src/main/java/com/light/finder/CameraLightFinderActivity.kt
+++ app/src/main/java/com/light/finder/CameraLightFinderActivity.kt
@@ -215,6 +215,7 @@ class CameraLightFinderActivity : BaseLightFinderActivity(), FragNavController.R
 
         if (!isBackButtonBlocked && screenNavigator.popFragmentNot()) {
             super.onBackPressed()
+            overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right)
         }
     }
 
