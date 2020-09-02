1994a02175c2587372b8a53a9a4ba63c3ffadbd4
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-09-02 11:03:52 +0200

    [REFACTOR] restoring success codes on BasedataSource

--- app/src/main/java/com/light/finder/data/source/BaseDataSource.kt
+++ app/src/main/java/com/light/finder/data/source/BaseDataSource.kt
@@ -2,6 +2,9 @@ package com.light.finder.data.source
 
 import com.google.gson.JsonParseException
 import com.light.data.Result
+import com.light.util.NO_CONTENT_CODE
+import com.light.util.NO_PRODUCTS_CODE
+import com.light.util.SUCCESSFUL_CODE
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.TimeoutCancellationException
@@ -17,7 +20,26 @@ abstract class BaseDataSource<Dto, DomainModel> {
         withContext(Dispatchers.IO) {
             val response = call()
             if (response.isSuccessful) {
-                Result.success(mapResultToDomainModel(response.body()!!), code = response.code())
+                when {
+                    response.code() == SUCCESSFUL_CODE -> {
+                        Result.success(
+                            mapResultToDomainModel(response.body()!!),
+                            code = response.code()
+                        )
+                    }
+                    response.code() == NO_CONTENT_CODE -> {
+                        Result.success(code = response.code())
+                    }
+                    response.code() == NO_PRODUCTS_CODE -> {
+                        Result.success(
+                            mapResultToDomainModel(response.body()!!),
+                            code = response.code()
+                        )
+                    }
+                    else -> {
+                        Result.success(code = response.code())
+                    }
+                }
             } else {
                 Result.badRequest(code = response.code())
             }
