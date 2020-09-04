cb4b410449c5adb692f9a72b6f7f7defe338bc99
Sarantis Tofas <sarantis.tofas@mobgen.com>
2020-09-04 12:28:58 +0200

    [ADD] Auth Interceptor to message remote [REFACTOR] Bearer Interceptor to accept token as a parameter [ADD] setAuthToken

--- app/src/main/java/com/light/finder/data/source/remote/services/MessageRemoteUtil.kt
+++ app/src/main/java/com/light/finder/data/source/remote/services/MessageRemoteUtil.kt
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.light.finder.BuildConfig
 import com.light.finder.common.HiddenAnnotationExclusionStrategy
+import com.light.finder.data.source.utils.BearerInterceptor
 import com.light.finder.data.source.utils.HttpErrorInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -11,8 +12,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object MessageRemoteUtil {
-
+//TODO pass bearer to interceptor from a local source
     private val okHttpClient: OkHttpClient = OkHttpClient.Builder().apply {
+        addInterceptor(BearerInterceptor("Bearer 49385fe86a8fd424a98bacbdd8845357"))
         addInterceptor(HttpErrorInterceptor())
         addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
     }.build()
--- app/src/main/java/com/light/finder/data/source/utils/BearerInterceptor.kt
+++ app/src/main/java/com/light/finder/data/source/utils/BearerInterceptor.kt
@@ -6,12 +6,16 @@ import okhttp3.Response
 import java.io.IOException
 
 //TODO Akis put this in MessageRemoteUtil Okhttp as interceptor
-class BearerInterceptor : Interceptor {
+class BearerInterceptor(private var authToken: String?) : Interceptor {
+    fun setAuthToken(authToken: String?) {
+        this.authToken = authToken
+    }
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder: Request.Builder = chain.request().newBuilder()
-        builder.addHeader("Authorization", "Bearer 49385fe86a8fd424a98bacbdd8845357")
+        builder.addHeader("Authorization", "Bearer $authToken")
         //TODO Akis change bearer token with the saved one from oauth call
         return chain.proceed(builder.build())
     }
 }
+
