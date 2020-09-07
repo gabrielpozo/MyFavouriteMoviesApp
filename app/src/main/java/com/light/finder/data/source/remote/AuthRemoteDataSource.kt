54ee46c33f874300d9fc1157670f623006357884
ege <ege.kuzubasioglu@accenture.com>
2020-09-07 13:43:03 +0300

    [FIX] conflicts

--- app/src/main/java/com/light/finder/data/source/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/com/light/finder/data/source/remote/AuthRemoteDataSource.kt
@@ -7,19 +7,18 @@ import com.light.finder.data.source.BaseDataSource
 import com.light.finder.data.source.remote.dto.BearerResultDto
 import com.light.finder.data.source.remote.services.OAuthRemoteUtil
 import com.light.source.remote.RemoteFetchAuthDataSource
-import com.light.util.IMMERSIVE_FLAG_TIMEOUT
+import com.light.util.BROWSING_FLAG_TIMEOUT
 import kotlinx.coroutines.withTimeout
 
-//TODO needs refactoring
-class AuthRemoteDataSource : BaseDataSource(), RemoteFetchAuthDataSource {
+class AuthRemoteDataSource : BaseDataSource<BearerResultDto, Bearer>(), RemoteFetchAuthDataSource {
     override suspend fun fetchBearerToken(): Result<Bearer> =
-        getResult(::mapAuthResultToDomainModel) {
-            withTimeout(IMMERSIVE_FLAG_TIMEOUT) {
+        getResult {
+            withTimeout(BROWSING_FLAG_TIMEOUT) {
                 OAuthRemoteUtil.service.fetchBearerTokenAsync()
             }
         }
 
-    private fun mapAuthResultToDomainModel(bearerResultDto: BearerResultDto): Bearer {
-        return mapAuthToDomain(bearerResultDto)
+    override fun mapResultToDomainModel(dtoObject: BearerResultDto): Bearer {
+        return mapAuthToDomain(dtoObject)
     }
 }
