4e41488910098e6c02ded76dc58a8721686fa25e
Sarantis Tofas <sarantis.tofas@mobgen.com>
2020-09-09 15:31:16 +0200

    [ADD] SingletonHolder class [REFACTOR] move SingletonHolder in Data module utils [REFACTOR] move FacebookAnalytics in analytics package

--- app/src/main/java/com/light/finder/data/source/remote/services/CartRemoteUtil.kt
+++ app/src/main/java/com/light/finder/data/source/remote/services/CartRemoteUtil.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.light.finder.BuildConfig
+import com.light.util.SingletonHolder
 import com.light.finder.data.source.utils.WebKitSyncCookieManager
 import com.light.finder.extensions.createCookieStore
 import okhttp3.JavaNetCookieJar
@@ -42,30 +43,4 @@ class CartRemoteUtil private constructor(val context: Context) {
         }
 
     companion object : SingletonHolder<CartRemoteUtil, Context>(::CartRemoteUtil)
-}
-
-open class SingletonHolder<out T : Any, in A>(creator: (A) -> T) {
-    private var creator: ((A) -> T)? = creator
-
-    @Volatile
-    private var instance: T? = null
-
-    fun getInstance(arg: A): T {
-        val i = instance
-        if (i != null) {
-            return i
-        }
-
-        return synchronized(this) {
-            val i2 = instance
-            if (i2 != null) {
-                i2
-            } else {
-                val created = creator!!(arg)
-                instance = created
-                creator = null
-                created
-            }
-        }
-    }
 }
