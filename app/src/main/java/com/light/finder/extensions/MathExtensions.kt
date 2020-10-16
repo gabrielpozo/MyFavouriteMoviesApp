487622eafd4099e4b1454891cc6e19a744e5f637
Sarantis Tofas <sarantis.tofas@mobgen.com>
2020-10-16 14:20:45 +0200

    [FIX] decimal crash

--- app/src/main/java/com/light/finder/extensions/MathExtensions.kt
+++ app/src/main/java/com/light/finder/extensions/MathExtensions.kt
@@ -1,4 +1,3 @@
 package com.light.finder.extensions
 
-fun Double.round(decimals: Int = 2): Double = "%.${decimals}f".format(this).toDouble()
 fun Int.pxToDp(density: Float): Int = this / density.toInt()
