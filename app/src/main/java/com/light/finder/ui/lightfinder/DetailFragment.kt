619903c22da57b41ff68a36814a332951b93b335
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2020-11-19 13:20:35 +0100

    [FIX] increase tappable area on help/information icons on detail screen fixed

--- app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
+++ app/src/main/java/com/light/finder/ui/lightfinder/DetailFragment.kt
@@ -109,7 +109,6 @@ class DetailFragment : BaseFragment() {
         } ?: throw Exception("Invalid Activity")
 
 
-
         setNavigationObserver()
         setDetailObservers()
         setLightStatusBar()
@@ -145,14 +144,15 @@ class DetailFragment : BaseFragment() {
     }
 
     private fun initInfoClickListeners() {
-        clickIntensity.setSafeOnClickListener {
+
+        infoIntensityTapView.setSafeOnClickListener {
             showInfoBottomSheetDialog(infoId = 1)
         }
-        clickColor.setSafeOnClickListener {
+        infoColorTapView.setSafeOnClickListener {
             showInfoBottomSheetDialog(infoId = 2)
         }
 
-        clickConnectivity.setSafeOnClickListener {
+        infoConnectivityTapView.setSafeOnClickListener {
             showInfoBottomSheetDialog(infoId = 3)
         }
     }
